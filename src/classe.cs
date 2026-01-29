9d4db3a692a16d4228cc22eba5a0ed28810c49ba
Felipe Augusto <felipementel@hotmail.com>
2026-01-28 21:38:24 -0300

    feat: Canal DEPLOY - gerando vulnerabilidade

--- src/classe.cs
+++ src/classe.cs
@@ -1,6 +1,6 @@
 public class CanalDEPLOY
 {
-    public string chave = "043d205a-8030-4cde-9892-313377a1c449";
+    public string chave = "389123yunfg34iug348r739ht";
 
     public string chave2 = "";
 
